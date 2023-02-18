dc1f969d652e89b52626701205e46fcd0c6b83f5
shieldgenerator7 <shieldgenerator7@yahoo.com>
2023-02-18 18:04:26 -0500

    Add Combo.cs

---
+++ Assets/Scripts/System/Combo.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Combo", menuName = "Combo")]
+public class Combo : ScriptableObject
+{
+    public List<Move> moves;
+}
